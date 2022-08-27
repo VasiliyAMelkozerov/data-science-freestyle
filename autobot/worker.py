d4ec7b8cad06c246b6226c5b1afe7df6043d4998
VasiliyAMelkozerov <vasiliy.melkozerov@gmail.com>
2022-08-27 14:48:40 -0500

    fifth checkpoint

--- autobot/worker.py
+++ autobot/worker.py
@@ -9,7 +9,7 @@ driver = webdriver.Chrome()
 #this takes us to our target website
 driver.get('https://www.linkedin.com')
 
-time.sleep(2)
+time.sleep(1)
 #we wait until the page is fully loaded to start inputting
 
 email = driver.find_element('xpath','//*[@id="session_key"]')
@@ -22,3 +22,13 @@ password.send_keys(p.password)
 
 login = driver.find_element('xpath','/html/body/main/section[1]/div/div/form/button')
 login.click()
+
+time.sleep(10)
+
+jobs = driver.find_element('xpath','/html/body')
+#its giving me an error and not clicking to where I want to go
+#alternatively what I can also do is go directly to the link after signing in
+#instead of having to go and do it the clicky clank way
+jobs.click()
+
+time.sleep(1)
