beca4cbe37f4a4fb6a7acdf1e7b6b19491dba680
VasiliyAMelkozerov <vasiliy.melkozerov@gmail.com>
2022-08-30 09:44:49 -0500

    base framework complete

---
+++ autobot/README.md
@@ -0,0 +1,13 @@
+# Wabba jakka auto job appa
+
+----------
+
+This here will host my information reguarding the project for building out exactly what I need to get this thing up and running.
+
+This project has a couple of different moving parts.
+1. Something that logs into linked in.
+    - That goes over to the jobs tab and works through each one of the entries.
+2. To autofill job applications based on personal information needed to be processed.
+    - It would also locally store a list of the company name as well as the postition.
+
+I have found Selenium so far as a good library to be able to traverse the internet with.
--- autobot/worker.py
+++ autobot/worker.py
@@ -1,9 +1,11 @@
 import selenium
 #this library has all sorts of web interaction stuff both for Java and python
 from selenium import webdriver
+#webdriver is what lets us do stuff
 import time
+#this is imported and used to wait for the websites to be done 
 import personal_information as p
-#this will be left commented out until I have a working model
+#using a personal moduele to be store personal information with it being easily taken
 
 driver = webdriver.Chrome()
 #this takes us to our target website
@@ -22,13 +24,35 @@ password.send_keys(p.password)
 
 login = driver.find_element('xpath','/html/body/main/section[1]/div/div/form/button')
 login.click()
+#so this here goes and completes the login process
+time.sleep(6)
+#now the time let's it all load in the way it needs to
+#there has been inconsistent loading times when it comes to loading in linkedin after signing in so for now time  is gonna work
 
-time.sleep(10)
+driver.get('https://www.linkedin.com/jobs/search/?currentJobId=3226173731&keywords=data%20analyst&refresh=true')
+#so from here we are on the page listings of data analyst positions 
 
-jobs = driver.find_element('xpath','/html/body')
-#its giving me an error and not clicking to where I want to go
-#alternatively what I can also do is go directly to the link after signing in
-#instead of having to go and do it the clicky clank way
-jobs.click()
+#from here I would like to click apply and open the results into a seperate tab
 
-time.sleep(1)
+#ahh yes back again to the selenium docs to open my results in a new tab
+
+time.sleep(5)
+"""
+OHHHHHHHHHHHHH hold on it instantly wants to input after loading the page bit that won't work
+
+give it some time
+"""
+
+
+#it just occured to me that clicking it might naturaully open up a new tab
+
+fbutton = driver.find_element('xpath','//*[@id="ember290"]')
+#okay so it does in fact open up in a brand new tab but then all of the code will be on just this one tab.....
+#but like what has happened here is that my selenium bot, Imma call him Mathew
+
+"""
+So Mathew logs me in and takes me to the page that I want to go he then clicks into the 
+first job listing on the entry. From there Mathew is kinda stuck on a new tab
+with not really a way to get back
+I haven't seen what Mathew does if I tell him to click through the first apply button
+"""
